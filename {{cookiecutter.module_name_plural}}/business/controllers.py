e48228f2be9cc861982f70bed1f1ab23a950840b
Hoang Manh Tien <tienhm@eway.vn>
2021-12-15 13:06:15 +0700

    chore: using cbv for controller init

--- {{cookiecutter.module_name_plural}}/business/controllers.py
+++ {{cookiecutter.module_name_plural}}/business/controllers.py
@@ -3,6 +3,15 @@
 
 Handle all request to {{cookiecutter.module_name_plural.title()}} resource. MUST follow FastAPI Router.
 """
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi_utils.cbv import cbv
+from sqlalchemy.orm import Session
+
+from src.utils import deps
 
 router = APIRouter()
+
+
+@cbv(router)
+class {{cookiecutter.module_name_singular.title()}}Controller:
+    db: Session = Depends(deps.get_db)
